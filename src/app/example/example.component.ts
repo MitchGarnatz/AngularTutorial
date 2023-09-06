8df8ed8023b83a3634926931f8322708ebe29083
Mitch Garnatz <garn1876@stthomas.edu>
2023-09-06 16:46:54 -0500

    Two-Way Binding Example

---
+++ src/app/example/example.component.ts
@@ -0,0 +1,12 @@
+// example.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  templateUrl: './example.component.html',
+  styleUrls: ['./example.component.css'],
+})
+export class ExampleComponent {
+  inputValue: string = ''; // Property to hold the data
+}
